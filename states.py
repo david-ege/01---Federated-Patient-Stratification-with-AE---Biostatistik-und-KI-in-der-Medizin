c0181e8e39387d280d268fdb74ec7af9f2daf051
4egelhof <david.egelhofer@studium.uni-hamburg.de>
2025-01-29 14:57:08 +0100

    added test logs, removed logging of model

--- states.py
+++ states.py
@@ -172,7 +172,7 @@ class ComputeState(AppState):
                 optimizer.step()
                 
                 total_loss += loss.item()
-                
+
             if (epoch + 1) % 10 == 0:
                     self.log(f"Epoch {epoch+1}, Loss: {total_loss / len(data_loader)}")
 
@@ -215,7 +215,7 @@ class AggregateState(AppState):
             # Update global model with aggregated weights
             self.log('Updating global model...')
             global_model = self.load('model')
-            self.log(global_model)
+            #self.log(global_model)
             for name, param in global_model.state_dict().items():
                 param.data = torch.tensor(agg_weights[name].mean())
 
